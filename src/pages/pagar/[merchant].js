f9c442b0d81e2bbd8f3e49b9014131a60c20e45d
aromeoes <aromeoes@gmail.com>
2023-04-12 10:36:40 -0300

    agregando web3 modal y wagmi

---
+++ src/pages/pagar/[merchant].js
@@ -0,0 +1,20 @@
+import { useRouter } from 'next/router';
+import { Web3Button } from '@web3modal/react'
+const Pagar = () => {
+    const router = useRouter();
+    const { merchant } = router.query;
+    console.log(router.query)
+
+    // Fetch data or perform operations based on the dynamicSlug value
+    // ...
+
+    return (
+        <div>
+            <h1>Pagar: {merchant}</h1>
+            {/* Render your component or data here */}
+            <Web3Button balance='show' icon='show' />
+        </div>
+    );
+};
+
+export default Pagar;
