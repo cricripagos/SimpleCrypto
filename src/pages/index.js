78618e83cf4ba8b0b8a6039d79a2b4acb097634d
haycarlitos <carlos.castilloperera@gmail.com>
2023-04-12 11:01:36 -0600

    fix: prevent hydration error

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,60 +1,48 @@
-import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
-import { Web3Modal } from '@web3modal/react'
-import { configureChains, createClient, WagmiConfig } from 'wagmi'
-import { arbitrum, mainnet, polygon } from 'wagmi/chains'
-import { Web3Button } from '@web3modal/react'
-import { useAccount } from 'wagmi'
+import {
+  EthereumClient,
+  w3mConnectors,
+  w3mProvider,
+} from "@web3modal/ethereum";
+import { Web3Button, Web3Modal } from "@web3modal/react";
+import { WagmiConfig, configureChains, createClient, useAccount } from "wagmi";
+import { arbitrum, mainnet, polygon } from "wagmi/chains";
 
-
-const chains = [arbitrum, mainnet, polygon]
-const projectId = '8579dab459fd9bbe2b74a2a67b2ae920'
-const { provider } = configureChains(chains, [w3mProvider({ projectId })])
+const chains = [arbitrum, mainnet, polygon];
+const projectId = "8579dab459fd9bbe2b74a2a67b2ae920";
+const { provider } = configureChains(chains, [w3mProvider({ projectId })]);
 const wagmiClient = createClient({
   autoConnect: true,
   connectors: w3mConnectors({ projectId, version: 1, chains }),
-  provider
-})
-const ethereumClient = new EthereumClient(wagmiClient, chains)
-
-import React from 'react'
-
-export const Address = () => {
-  const { address, isConnecting, isDisconnected } = useAccount()
-  if (isConnecting) return <div>Connecting…</div>
-  if (isDisconnected) return <div>Disconnected</div>
-  return <div>{address}</div>
-}
-
+  provider,
+});
+const ethereumClient = new EthereumClient(wagmiClient, chains);
 
 export const Erc20 = ({ address }) => {
-  return (
-    <div>Erc20 </div>
-  )
-}
-
-
+  return <div>Erc20 </div>;
+};
 
 const Pagar = () => {
-  return (
-    <div>index</div>
-  )
-}
+  return <div>index</div>;
+};
+
+export const Address = () => {
+  const { address, isConnecting, isDisconnected } = useAccount();
+  if (isConnecting) return <div>Connecting…</div>;
+  if (isDisconnected) return <div>Disconnected</div>;
+  return <div>{address}</div>;
+};
 
 export default function App() {
   return (
     <>
       <WagmiConfig client={wagmiClient}>
         {/*<HomePage />*/}
-        lala
-        return <Web3Button balance='show' icon='show' />
+        lala return <Web3Button balance="show" icon="show" />
         <Pagar />
         <Address />
       </WagmiConfig>
 
-
-
       <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
     </>
-  )
+  );
 }
-
