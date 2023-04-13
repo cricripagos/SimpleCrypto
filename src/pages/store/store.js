5565d5793310f2ffed6c29702a3594085a97367e
Rocio Jacob <rochijacob@gmail.com>
2023-04-13 20:23:47 +0200

    message

---
+++ src/pages/store/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+import sample from './reducers/sample';
+
+
+//Para cada caso que queremos usar podemos crear un reducer. Por ejemplo por temas. Payment, User, etc
+export const store = configureStore({
+    reducer: {
+        sample: sample
+    }
+})
