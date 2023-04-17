ed6331d95ac3abdd9fd7a9f1ca2663df42bd3ada
aromeoes <aromeoes@gmail.com>
2023-04-17 11:20:44 -0300

    mensajes corregidos de prueba de compra

--- src/components/EvmFlowDemo.js
+++ src/components/EvmFlowDemo.js
@@ -1,25 +1,23 @@
-import React, { useState, useEffect } from 'react'
-import PagarHandler from './PagarHandler'
+import React, { useState } from 'react'
 import dynamic from 'next/dynamic'
 import DebuggerPagar from './DebuggerPagar'
 const DebuggerBalances = dynamic(() => import('../components/DebuggerBalances'), {
   ssr: false,
 })
 
-const EvmFlowDemo = ({token,contract_address, payer_address, beneficiary_address,amount,  current_blockchain}) => {
-    //TODO esta variable balances, deberia pasarse a store/redux/context 
-    console.log(beneficiary_address)
-    const [balances, setBalances ] = useState(false)
-   return (
+const EvmFlowDemo = ({ payer_address, beneficiary_address, amount, current_blockchain }) => {
+  //TODO esta variable balances, deberia pasarse a store/redux/context 
+  const [balances, setBalances] = useState(false)
+  return (
     <>
-        <div>EvmFlowDemo</div>
-        <DebuggerPagar
-            balances={balances}
-            amount={amount}
-            current_blockchain={current_blockchain}
-            beneficiary_address={beneficiary_address}
-        />
-        {/* 
+      <div>EvmFlowDemo</div>
+      <DebuggerPagar
+        balances={balances}
+        amount={amount}
+        current_blockchain={current_blockchain}
+        beneficiary_address={beneficiary_address}
+      />
+      {/* 
         <PagarHandler
           token
           chosen_blockchain
@@ -31,7 +29,7 @@ const EvmFlowDemo = ({token,contract_address, payer_address, beneficiary_address
           chain_id
         />
         */}
-        <DebuggerBalances {... { current_blockchain, payer_address, setBalances  }} />
+      <DebuggerBalances {... { current_blockchain, payer_address, setBalances }} />
 
     </>
   )
