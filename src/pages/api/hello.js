a8b78fc8a0dea143d699e6fa125c54e3f42722d7
haycarlitos <carlos.castilloperera@gmail.com>
2023-04-18 09:20:56 -0600

    build: add ui validations

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  const input = JSON.parse(req.body).amount;
+
+  console.log("Procesando ", input, " satoshis");
+
+  res.status(200).json({ name: "John Doe" });
 }
--- src/pages/lnd/index.js
+++ src/pages/lnd/index.js
@@ -1,30 +1,111 @@
 import { useState } from "react";
 
 const App = () => {
-  const [value, setValue] = useState("");
+  const [amount, setAmount] = useState("");
+  const [isProcessing, setIsProcessing] = useState(false);
 
   const handleChange = (event) => {
     const result = event.target.value.replace(/\D/g, "");
 
-    setValue(result);
+    setAmount(result);
+  };
+  /*
+  const sleep = (ms) => {
+    const promise = new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+    return promise;
+  };
+*/
+  const sleep = (ms) => {
+    return setTimeout(ms);
   };
 
-  console.log(value);
-  console.log(typeof value);
-  console.log(Number(value));
+  const generateInvoice = async () => {
+    if (amount.length == 0) {
+      alert("Ingresa una cantidad");
+      return;
+    }
+
+    console.log("Generating invoice...");
+
+    // Set loading has started
+    setIsProcessing(true);
+
+    const response = await fetch("/api/hello", {
+      method: "POST",
+      headers: {
+        "Content-Type": "image/jpeg",
+      },
+      body: JSON.stringify({ amount }),
+    });
+
+    await sleep(5000);
+
+    const data = await response.json();
+    console.log(data);
+
+    if (response.status === 503) {
+      // Set the estimated_time property in state
+      console.log("validando nodo");
+      return;
+    }
+
+    if (!response.ok) {
+      console.log(`Error: ${data}`);
+      setIsProcessing(false);
+
+      return;
+    }
+
+    setAmount("");
+    setIsProcessing(false);
+  };
 
   return (
-    <div>
+    <>
       <div>
-        <h1>Cantidad en satoshis a pagar:</h1>
+        <div>
+          <h1>Cantidad en satoshis a pagar:</h1>
+        </div>
+        <br></br>
+        <input
+          type="text"
+          placeholder="Ingresa la cantidad"
+          value={amount}
+          onChange={handleChange}
+        />
+        <br></br>
       </div>
-      <input
-        type="text"
-        placeholder="Your fav number"
-        value={value}
-        onChange={handleChange}
-      />
-    </div>
+      <br></br>
+      {amount.length > 0 && (
+        <div>
+          <p>Total en Satoshis: {amount}</p>
+        </div>
+      )}
+      <br></br>
+      <div>
+        <a onClick={generateInvoice}>
+          {/* Tweak to show a loading indicator */}
+          <div>
+            {isProcessing ? (
+              <p className="text-red-500">Procesando</p>
+            ) : (
+              <p className="text-sky-500 hover:text-sky-600">Pagar</p>
+            )}
+          </div>
+        </a>
+      </div>
+      <br></br>
+      <textarea
+        style={{ resize: "none" }}
+        rows="9"
+        cols="32"
+        value={amount}
+        readOnly
+      ></textarea>
+      <br></br>
+    </>
   );
 };
 
