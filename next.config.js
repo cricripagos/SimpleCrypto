44611b3cc93c092f88fbb92b3eff5dd2abafef4f
haycarlitos <carlos.castilloperera@gmail.com>
2023-04-19 14:39:39 -0600

    refactor: remove unnecessary config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  serverRuntimeConfig: {
-    PROJECT_ROOT: __dirname,
-  },
 };
 
 module.exports = nextConfig;
