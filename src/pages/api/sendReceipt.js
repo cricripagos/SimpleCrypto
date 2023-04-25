ac0d9f8c5553f7ca8cd39bb7fbacc81f196fb31b
haycarlitos <carlos.castilloperera@gmail.com>
2023-04-25 11:26:20 -0600

    build: add twilio template{

--- src/pages/api/sendReceipt.js
+++ src/pages/api/sendReceipt.js
@@ -3,13 +3,19 @@ export default async function generateInvoice(req, res) {
   const authToken = process.env.NEXT_PUBLIC_TWILIO_TOKEN;
   const client = require("twilio")(accountSid, authToken);
   const amount = JSON.parse(req.body).amount;
-  const message = "Hola, Tule! te acaban de pagar " + amount + " satoshis. 🤑";
+  const message =
+    "Hola, Tule acabas de recibir " +
+    amount +
+    " satoshis.\nAcá puedes ver la transacción https://blockstream.info/tx/";
+
+  console.log(message);
 
   client.messages
     .create({
       body: message,
-      from: "whatsapp:+14155238886",
-      to: "whatsapp:+5491154865055",
+      //  from: "whatsapp:+14155238886",
+      from: "whatsapp:+16319003288",
+      to: "whatsapp:+5215544944051",
     })
     .then((message) => {
       console.log(message.sid);
