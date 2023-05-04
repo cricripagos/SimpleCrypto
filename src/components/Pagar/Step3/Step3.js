be7cee675536751411b2ecc96db246641f87f66a
haycarlitos <carlos.castilloperera@gmail.com>
2023-05-04 08:05:59 -0600

    refactor: export header and evmtokers as components

--- src/components/Pagar/Step3/Step3.js
+++ src/components/Pagar/Step3/Step3.js
@@ -1,26 +1,22 @@
-import React, { useEffect } from 'react'
-import { Footer, FooterPay, InputComponent, Layout } from "@components/components"
-import EvmTokens from '@components/EvmTokens/EvmTokens'
+import { setBtnDisabled } from "@/store/reducers/interactions";
+import { EvmTokens, FooterPay, Header, Layout } from "@components/components";
 import { Web3Button } from "@web3modal/react";
-import Header from '@components/Header/Header';
-import { useDispatch, useSelector } from 'react-redux';
-import { useAccount } from 'wagmi';
-import { setBtnDisabled } from '@/store/reducers/interactions';
-
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useAccount } from "wagmi";
 
 const Step3 = () => {
-  const { payment_method } = useSelector(state => state.order)
-  const { address } = useAccount()
-  const dispatch = useDispatch()
-
+  const { payment_method } = useSelector((state) => state.order);
+  const { address } = useAccount();
+  const dispatch = useDispatch();
 
   useEffect(() => {
     if (address !== undefined && payment_method !== null) {
-      dispatch(setBtnDisabled(false))
+      dispatch(setBtnDisabled(false));
     } else {
-      dispatch(setBtnDisabled(true))
+      dispatch(setBtnDisabled(true));
     }
-  }, [payment_method, address])
+  }, [payment_method, address]);
 
   return (
     <Layout>
@@ -28,14 +24,17 @@ const Step3 = () => {
         <div className="flex bg-green-1 rounded-b-lg justify-center items-center flex-col pb-5 px-7">
           <Header />
         </div>
-        <div className="flex flex-col h-full overflow-scroll overflow-x-hidden overflow-y-scroll scrollbar-hide px-7 mb-24 py-3" id='scrollbox'>
+        <div
+          className="flex flex-col h-full overflow-scroll overflow-x-hidden overflow-y-scroll scrollbar-hide px-7 mb-24 py-3"
+          id="scrollbox"
+        >
           <Web3Button balance="show" icon="show" />
           <EvmTokens />
         </div>
-        <FooterPay btn_msg='Pagar' />
+        <FooterPay btn_msg="Pagar" />
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default Step3
+export default Step3;
